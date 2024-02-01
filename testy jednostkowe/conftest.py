d8491c91561e56efd417087c02e2ad133935f109
gilewskadominika <gilewskadominika@wp.pl>
2024-02-02 00:10:29 +0100

    few tests

--- testy jednostkowe/conftest.py
+++ testy jednostkowe/conftest.py
@@ -1,4 +1,4 @@
-from datetime import datetime, time
+from datetime import datetime, time, date
 
 import pytest
 from django.contrib.auth.models import User
@@ -8,61 +8,81 @@ from organize_your_pet.models import Pet, Clinic, Doctor, AvailableDate, Visit
 
 @pytest.fixture
 def user():
-    return User.objects.create(username='test')
+    return User.objects.create(username='test', password='DupaDupa')
 
 
 @pytest.fixture
-def pet():
+def pets(user):
     x = [
-        Pet.objects.create(name='Azor', species='Pies', breed='Mieszaniec', gender='Samiec', birth_date=2022-12-20,
-                                weight=2, chip=True),
-        Pet.objects.create(name='Kazor', species='Pies', breed='Mieszaniec', gender='Samica', birth_date=2022-10-28,
-                                weight=5, chip=False)]
+        Pet.objects.create(name='Azor', species='Pies', other_species='brak', breed='Mieszaniec', gender='Samiec',
+                           birth_date=date(2022, 12, 20), weight=2, chip=True, owner=user),
+        Pet.objects.create(name='Kazor', species='Pies', other_species='brak', breed='Mieszaniec', gender='Samica',
+                           birth_date=date(2022, 10, 28), weight=5, chip=False, owner=user)]
     return x
 
 
 @pytest.fixture
-def clinic():
+def clinics():
     x = [
-        Clinic.objects.create(name='Vetka', address='ul.Główna 7', city='Wiskitki', phone_number=555-369-285),
-        Clinic.objects.create(name='Beatka', address='ul.Górna 157', city='Warszawa', phone_number=575-969-277)
+        Clinic.objects.create(name='Vetka', address='ul.Główna 7', city='Wiskitki', phone_number=555369285),
+        Clinic.objects.create(name='Beatka', address='ul.Górna 157', city='Warszawa', phone_number=575969277)
     ]
     return x
 
 
 @pytest.fixture
-def doctor(clinic):
+def doctors(clinics):
     x = [
-        Doctor.objects.create(first_name='Adam', last_name='Azorkiewicz', specialization='Ortopeda', clinic_id=clinic[0].id),
-        Doctor.objects.create(first_name='Kamil', last_name='Azork', specialization='Okulista', clinic_id=clinic[0].id),
-        Doctor.objects.create(first_name='Abraham', last_name='Kiełkiewicz', specialization='Internista', clinic_id=clinic[0].id),
-        Doctor.objects.create(first_name='Anna', last_name='Azot', specialization='Ortopeda', clinic_id=clinic[1].id),
-        Doctor.objects.create(first_name='Alina', last_name='Bełkiewicz', specialization='Okulista', clinic_id=clinic[1].id),
-        Doctor.objects.create(first_name='Adam', last_name='Tramtyn', specialization='Internista', clinic_id=clinic[1].id),
+        Doctor.objects.create(first_name='Adam', last_name='Azorkiewicz', specialization='Ortopeda',
+                              clinic_id=clinics[0].id),
+        Doctor.objects.create(first_name='Kamil', last_name='Azork', specialization='Okulista',
+                              clinic_id=clinics[0].id),
+        Doctor.objects.create(first_name='Abraham', last_name='Kiełkiewicz', specialization='Internista',
+                              clinic_id=clinics[0].id),
+        Doctor.objects.create(first_name='Anna', last_name='Azot', specialization='Ortopeda',
+                              clinic_id=clinics[1].id),
+        Doctor.objects.create(first_name='Alina', last_name='Bełkiewicz', specialization='Okulista',
+                              clinic_id=clinics[1].id),
+        Doctor.objects.create(first_name='Adam', last_name='Tramtyn', specialization='Internista',
+                              clinic_id=clinics[1].id),
     ]
     return x
 
 
 @pytest.fixture
-def available_date(doctor):
+def available_dates(doctors):
     x = [
-        AvailableDate.objects.create(date=2024-1-30, start_time=time(15,30), end_time=time(16, 00), clinic_id=doctor[0].clinic_id, doctor_id=doctor[0].id),
-        AvailableDate.objects.create(date=2024-1-30, start_time=time(15,30), end_time=time(16, 00), clinic_id=doctor[1].clinic_id, doctor_id=doctor[1].id),
-        AvailableDate.objects.create(date=2024-1-30, start_time=time(16,30), end_time=time(17, 00), clinic_id=doctor[2].clinic_id, doctor_id=doctor[2].id),
-        AvailableDate.objects.create(date=2024-1-30, start_time=time(16,30), end_time=time(17, 00), clinic_id=doctor[3].clinic_id, doctor_id=doctor[3].id),
-        AvailableDate.objects.create(date=2024-1-30, start_time=time(15,30), end_time=time(16, 00), clinic_id=doctor[4].clinic_id, doctor_id=doctor[4].id),
-        AvailableDate.objects.create(date=2024-1-30, start_time=time(15,30), end_time=time(16, 00), clinic_id=doctor[5].clinic_id, doctor_id=doctor[5].id),
-        AvailableDate.objects.create(date=2024-1-31, start_time=time(14,30), end_time=time(15, 00), clinic_id=doctor[0].clinic_id, doctor_id=doctor[0].id),
+        AvailableDate.objects.create(date=date(2024, 1, 30), start_time=time(15,30),
+                                     end_time=time(16, 00), clinic_id=doctors[0].clinic_id,
+                                     doctor_id=doctors[0].id),
+        AvailableDate.objects.create(date=date(2024, 1, 30), start_time=time(15,30),
+                                     end_time=time(16, 00), clinic_id=doctors[1].clinic_id,
+                                     doctor_id=doctors[1].id),
+        AvailableDate.objects.create(date=date(2024, 1, 30), start_time=time(16,30),
+                                     end_time=time(17, 00), clinic_id=doctors[2].clinic_id,
+                                     doctor_id=doctors[2].id),
+        AvailableDate.objects.create(date=date(2024, 1, 30), start_time=time(16,30),
+                                     end_time=time(17, 00), clinic_id=doctors[3].clinic_id,
+                                     doctor_id=doctors[3].id),
+        AvailableDate.objects.create(date=date(2024, 1, 30), start_time=time(15,30),
+                                     end_time=time(16, 00), clinic_id=doctors[4].clinic_id,
+                                     doctor_id=doctors[4].id),
+        AvailableDate.objects.create(date=date(2024, 1, 30), start_time=time(15,30),
+                                     end_time=time(16, 00), clinic_id=doctors[5].clinic_id,
+                                     doctor_id=doctors[5].id),
+        AvailableDate.objects.create(date=date(2024, 1, 30), start_time=time(14,30),
+                                     end_time=time(15, 00), clinic_id=doctors[0].clinic_id,
+                                     doctor_id=doctors[0].id),
     ]
     return x
 
 
 @pytest.fixture
-def visit(available_date, pet):
+def visits(available_dates, pets):
     x = [
-        Visit.objects.create(description='łapaała', available_date_id=available_date[0].id, pet_id=pet[0].id),
-        Visit.objects.create(description='łapaała', available_date_id=available_date[1].id, pet_id=pet[1].id),
-        Visit.objects.create(description='łapaała', available_date_id=available_date[2].id, pet_id=pet[0].id),
-        Visit.objects.create(description='łapaała', available_date_id=available_date[3].id, pet_id=pet[1].id),
+        Visit.objects.create(description='łapaała', available_date_id=available_dates[0].id, pet_id=pets[0].id),
+        Visit.objects.create(description='łapaała', available_date_id=available_dates[1].id, pet_id=pets[1].id),
+        Visit.objects.create(description='łapaała', available_date_id=available_dates[2].id, pet_id=pets[0].id),
+        Visit.objects.create(description='łapaała', available_date_id=available_dates[3].id, pet_id=pets[1].id),
     ]
     return x
--- testy jednostkowe/tests.py
+++ testy jednostkowe/tests.py
@@ -1,9 +1,287 @@
-import pytest
+from datetime import date, datetime
 
-from organize_your_pet.models import Pet
+import pytest
+from django.test import Client
+from django.urls import reverse
+
+from accounts.forms import RegistrationForm
+from organize_your_pet.forms import AddPetForm
+from organize_your_pet.models import Pet, Clinic, Doctor, AvailableDate, Visit
+
+# sprawdzenie fixtures
 
 
 @pytest.mark.django_db
-def test_pet_count(pet):
+def test_pet_count(pets):
     x = Pet.objects.all()
     assert x.count() == 2
+
+
+@pytest.mark.django_db
+def test_clinic_count(clinics):
+    x = Clinic.objects.all()
+    assert x.count() == 2
+
+
+@pytest.mark.django_db
+def test_doctor_count(doctors):
+    x = Doctor.objects.all()
+    assert x.count() == 6
+
+
+@pytest.mark.django_db
+def test_available_date_count(available_dates):
+    x = AvailableDate.objects.all()
+    assert x.count() == 7
+
+
+@pytest.mark.django_db
+def test_visit_count(visits):
+    x = Visit.objects.all()
+    assert x.count() == 4
+
+# ----------------------------------------------------------------
+
+@pytest.mark.django_db
+def test_list_pets(pets, user):
+    client = Client()
+    client.force_login(user)
+    url = reverse('pets_list')
+    response = client.get(url)
+    assert response.status_code == 200
+    assert response.context['list_elements'].count() == len(pets)
+    for pet in pets:
+        assert pet in response.context['list_elements']
+
+
+@pytest.mark.django_db
+def test_search_pet(pets, user):
+    client = Client()
+    client.force_login(user)
+    url = reverse('pets_list')
+    url = f'{url}?name=k'
+    response = client.get(url)
+    assert response.status_code == 200
+    assert response.context['list_elements'].count() == 1
+    assert response.context['list_elements'][0] == pets[1]
+
+# ----------------------------------------------------------------
+
+
+@pytest.mark.django_db
+def test_add_pet_get(user):
+    client = Client()
+    client.force_login(user)
+    url = reverse('add_pet')
+    response = client.get(url)
+    assert response.status_code == 200
+    assert isinstance(response.context['form'], AddPetForm)
+
+
+@pytest.mark.django_db
+def test_add_pet_post(user):
+    client = Client()
+    client.force_login(user)
+    url = reverse('add_pet')
+    data = {
+        'name': 'Azor',
+        'species': 'Pies',
+        'breed': 'Mieszaniec',
+        'gender': 'Samiec',
+        'birth_date': '2022-12-20',
+        'weight': 2.0,
+        'chip': True
+    }
+    response = client.post(url, data)
+    assert response.status_code == 302
+    assert response.url == reverse('pets_list')
+    assert Pet.objects.get(name=data['name'],
+                           species=data['species'],
+                           breed=data['breed'],
+                           gender=data['gender'],
+                           birth_date=data['birth_date'],
+                           weight=data['weight'],
+                           chip=data['chip'])
+
+
+@pytest.mark.django_db
+def test_add_pet_not_login():
+    client = Client()
+    url = reverse('add_pet')
+    response = client.get(url)
+    assert response.status_code == 302
+    assert response.url.startswith(reverse('login_view'))
+
+# ----------------------------------------------------------------
+
+
+@pytest.mark.django_db
+def test_detail_pet(user, pets):
+    pet = Pet.objects.first()
+    client = Client()
+    client.force_login(user)
+    url = reverse('pet_info', kwargs={'pk': pet.pk})
+    response = client.get(url)
+    # response = client.get(f'/organize_your_pet/pet/{pets.pk}/')
+    assert response.status_code == 200
+    for field in ('name', 'species', 'other_species', 'breed', 'gender', 'birth_date', 'weight', 'chip', 'owner'):
+        assert field in response.data
+
+# ----------------------------------------------------------------
+
+
+@pytest.mark.django_db
+def test_modify_pet(user, pets):
+    pet = Pet.objects.first()
+    client = Client()
+    client.force_login(user)
+    url = reverse('modify_pet', kwargs={'pk': pet.pk})
+    response = client.get(url)
+    pet_data = response.data
+    new_pet_name = 'Kaziu'
+    pet_data['name'] = new_pet_name
+    response = client.patch(url)
+    assert response.status_code == 200
+    pet_object = Pet.objects.get(pk=pet.pk)
+    assert pet_object.name == new_pet_name
+
+# ----------------------------------------------------------------
+
+
+@pytest.mark.django_db
+def test_delete_pet(user, pets):
+    pet = Pet.objects.first()
+    client = Client()
+    client.force_login(user)
+    url = reverse('delete_pet', kwargs={'pk': pet.pk})
+    response = client.delete(url)
+    assert response.status_code == 204
+    pet_pks = [pet.pk for pet in Pet.objects.all()]
+    assert pet.pk not in pet_pks
+
+# ----------------------------------------------------------------
+
+
+@pytest.mark.django_db
+def test_list_clinics(clinics, user):
+    client = Client()
+    client.force_login(user)
+    url = reverse('clinics_list')
+    response = client.get(url)
+    assert response.status_code == 200
+    assert response.context['list_elements'].count() == len(clinics)
+    for clinic in clinics:
+        assert clinic in response.context['list_elements']
+
+
+@pytest.mark.django_db
+def test_list_clinics_not_login(clinics):
+    client = Client()
+    url = reverse('clinics_list')
+    response = client.get(url)
+    assert response.status_code == 302
+    assert response.url.startswith(reverse('login_view'))
+
+# ----------------------------------------------------------------
+
+
+@pytest.mark.django_db
+def test_list_doctors(doctors, user):
+    client = Client()
+    client.force_login(user)
+    url = reverse('doctors_list', kwargs={'clinic_id': doctors[0].clinic_id})
+    response = client.get(url)
+    assert response.status_code == 200
+    assert response.context['list_elements'].count() == len(doctors)
+    for doctor in doctors:
+        assert doctor in response.context['list_elements']
+
+
+@pytest.mark.django_db
+def test_list_doctors_not_login(doctors):
+    client = Client()
+    url = reverse('doctors_list', kwargs={'clinic_id': doctors[0].clinic_id})
+    response = client.get(url)
+    assert response.status_code == 302
+    assert response.url.startswith(reverse('login_view'))
+
+# ----------------------------------------------------------------
+
+
+@pytest.mark.django_db
+def test_list_visits(visits, user):
+    client = Client()
+    client.force_login(user)
+    url = reverse('visits_list')
+    response = client.get(url)
+    assert response.status_code == 200
+    assert response.context['list_elements'].count() == len(visits)
+    for visit in visits:
+        assert visit in response.context['list_elements']
+
+
+@pytest.mark.django_db
+def test_list_visits_not_login(visits):
+    client = Client()
+    url = reverse('visits_list')
+    response = client.get(url)
+    assert response.status_code == 302
+    assert response.url.startswith(reverse('login_view'))
+
+# ----------------------------------------------------------------
+
+
+@pytest.mark.django_db
+def test_visit_detail(visits, user):
+    visit = Visit.objects.first()
+    client = Client()
+    client.force_login(user)
+    url = reverse('visit_info', kwargs={'pk': visit.pk})
+    response = client.get(url)
+    assert response.status_code == 200
+    for field in 'description':
+        assert field in response.data
+
+# ----------------------------------------------------------------
+
+
+@pytest.mark.django_db
+def test_registration_user():
+    client = Client()
+    url = reverse('registration_view')
+    response = client.get(url)
+    assert response.status_code == 200
+    assert isinstance(response.context['form'], RegistrationForm)
+
+
+@pytest.mark.django_db
+def test_registration_user_if_login(user):
+    client = Client()
+    client.force_login(user)
+    url = reverse('registration_view')
+    response = client.get(url)
+    assert response.status_code == 302
+    assert response.url.startswith(reverse('dashboard'))
+
+# ----------------------------------------------------------------
+
+
+@pytest.mark.django_db
+def test_login_user_if_login(user):
+    client = Client()
+    client.force_login(user)
+    url = reverse('login_view')
+    response = client.get(url)
+    assert response.status_code == 302
+    assert response.url.startswith(reverse('dashboard'))
+
+# ----------------------------------------------------------------
+
+
+@pytest.mark.django_db
+def test_logout_user_not_login():
+    client = Client()
+    url = reverse('logout_view')
+    response = client.get(url)
+    assert response.status_code == 302
+    assert response.url.startswith(reverse('login_view'))
